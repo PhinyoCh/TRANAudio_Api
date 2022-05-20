a6ba6942d077ccb8d1cba2d92e57c9e47c499478
Phinyo Chaengchit (TNoS) <101833411+PhinyoCh@users.noreply.github.com>
2022-05-20 23:15:42 +0700

    Add files via upload

---
+++ db_project.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 20, 2022 at 04:15 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE IF NOT EXISTS `member` (
+  `member_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `s_name` varchar(50) NOT NULL,
+  `l_name` varchar(50) NOT NULL,
+  `user_role` int NOT NULL,
+  `active` int NOT NULL,
+  `create_by` int NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_by` int NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`member_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE IF NOT EXISTS `room` (
+  `room_id` int NOT NULL AUTO_INCREMENT,
+  `room_name` varchar(50) NOT NULL,
+  `mac_address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ip_address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `floor` int NOT NULL,
+  `active` int NOT NULL,
+  `create_by` int NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_by` int NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`room_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
